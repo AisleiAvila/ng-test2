eb815fcfd5bcdc67988a9ece261cf340fdf28575
AisleiAvila <aislei@outlook.com.br>
2021-06-21 08:36:09 -0300

    Melhorias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { PhotoBoardService } from './shared/components/photo-board/services/photo-board.service';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Photo } from './shared/components/photo-board/interfaces/photo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Angular testing';
-  public likes = 0;
 
-  public like(): void {
-    this.likes++;
+  public photos$: Observable<Photo[]>;
+
+  constructor(private service: PhotoBoardService) {}
+  
+  public ngOnInit(): void {
+    this.photos$ = this.service.getPhotos();
   }
+
 }
---
+++ src/app/shared/components/photo-board/photo-board.module.ts
@@ -0,0 +1,18 @@
+import { PhotoBoardComponent } from './photo-board.component';
+import { PhotoFrameModule } from './../photo-frame/photo-frame.module';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { PhotoBoardService } from './services/photo-board.service';
+
+@NgModule({
+    declarations: [
+        PhotoBoardComponent
+    ],
+    imports: [
+        CommonModule,
+        PhotoFrameModule
+    ],
+    exports: [PhotoBoardComponent],
+    providers: [PhotoBoardService]
+})
+export class PhotoBoardModule { }
